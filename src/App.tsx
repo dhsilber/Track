b430302f6f1d7d1d224296dea2829ee6a71fb7ad
David Silber <ddragontech@gmail.com>
2024-08-25 19:15:03 -0400

    Store pain type

--- src/App.tsx
+++ src/App.tsx
@@ -18,43 +18,62 @@ const enum Part {
   Back = "Back",
 }
 
+const enum Pain {
+  Ache = "Ache",
+  Pain = "Pain",
+  Burning = "Burning",
+  Sharp = "Sharp",
+  Other = "Other",
+}
+
 interface Track {
   timestamp: number
   painLevel: number
   side: Side
   part: Part
+  pain: Pain[]
 }
 
-interface TrackSet {
+interface TrackGroup {
   tracked: Track[]
 }
 
-const defaultTrackData: TrackSet = {
-  tracked: [
-      { 
-        timestamp: 0,
-        painLevel: 0,
-        side: Side.Unknown,
-        part: Part.Unknown,
-      },
-  ]
+const defaultTrackData: TrackGroup = {
+  tracked: []
 }
 
 function App() {
   const [painLevel, setPainLevel ] = useState(0)
   const [side, setSide ] = useState<Side>(Side.Unknown)
   const [part, setPart ] = useState<Part>(Part.Unknown)
-  const [getStorage, setStorage] = useLocalStorageState("track", {
+  const [pain, setPain ] = useState<Set<Pain>>(new Set)
+  const [getStorage, setStorage] = useLocalStorageState("track1", {
     defaultValue: defaultTrackData
   })
 
+  function addPain(painValue: Pain) {
+    setPain(new Set(pain).add(painValue))
+  }
+
+  function removePain(painValue: Pain) {
+    const hurts = new Set(pain)
+    hurts.delete(painValue)
+    setPain(hurts)
+  }
+
+  function togglePain(painValue: Pain) {
+    pain.has(painValue) ? removePain(painValue) : addPain(painValue)
+  }
+
   function store() {
-    let data = getStorage
+    const data = getStorage
+
     data.tracked.unshift( { 
       timestamp: Date.now(),
       painLevel: painLevel,
       side: side,
       part: part,
+      pain: Array.from(pain),
      })
     setStorage( data )
   }
@@ -64,7 +83,8 @@ function App() {
     const month = date.toLocaleString('en-us', {month:'short'})
     const time = `${(date.getHours()+"").padStart(2,'0')}:${(date.getMinutes()+"").padStart(2,'0')}`
     const timestamp = `${month} ${date.getDate()} ${time}`
-    const rest = `Pain: ${item.painLevel} - ${item.side} ${item.part}`
+    const painString = item.pain.join( ', ' )
+    const rest = `Pain: ${item.painLevel} - ${item.side} ${item.part} - ${painString}`
     return `${timestamp} - ${rest}`
   }
 
@@ -96,21 +116,6 @@ function App() {
           />
         </div>
       </div>
-      {Part.Back !== part && <form>
-        <label>
-          <input type={'checkbox'} checked={Side.Left == side} onClick={()=>setSide(Side.Left === side ? Side.Unknown : Side.Left)}/>
-          Left
-        </label>
-        <label>
-          <input type={'checkbox'} checked={Side.Right == side} onClick={()=>setSide(Side.Right === side ? Side.Unknown : Side.Right)}/>
-          Right
-        <label>
-          <input type={'checkbox'} checked={Side.Both == side} onClick={()=>setSide(Side.Both === side ? Side.Unknown : Side.Both)}/>
-          Both
-        </label>
-        </label>
-      </form>}
-      {Part.Back === part && <div>Only</div>}
       <form>
         <label>
           <input type={'checkbox'} checked={Part.Back == part} onClick={()=>{
@@ -126,6 +131,39 @@ function App() {
         <label>
           <input type={'checkbox'} checked={Part.Knee == part} onClick={()=>setPart(Part.Knee === part ? Part.Unknown : Part.Knee)}/>
           Knee
+        </label>
+      </form>
+      {Part.Back !== part && <form>
+        <label>
+          <input type={'checkbox'} checked={Side.Left == side} onClick={()=>setSide(Side.Left === side ? Side.Unknown : Side.Left)}/>
+          Left
+        </label>
+        <label>
+          <input type={'checkbox'} checked={Side.Right == side} onClick={()=>setSide(Side.Right === side ? Side.Unknown : Side.Right)}/>
+          Right
+        </label>
+        <label>
+          <input type={'checkbox'} checked={Side.Both == side} onClick={()=>setSide(Side.Both === side ? Side.Unknown : Side.Both)}/>
+          Both
+        </label>
+      </form>}
+      {Part.Back === part && <div>Only</div>}
+      <form>
+        <label>
+          <input type={'checkbox'} checked={pain.has(Pain.Ache)} onClick={()=>{togglePain(Pain.Ache)}} />
+          Ache
+        </label>
+        <label>
+          <input type={'checkbox'} checked={pain.has(Pain.Pain)} onClick={()=>{togglePain(Pain.Pain)}} />
+          Pain
+        </label>
+        <label>
+          <input type={'checkbox'} checked={pain.has(Pain.Burning)} onClick={()=>{togglePain(Pain.Burning)}} />
+          Burning
+        </label>
+        <label>
+          <input type={'checkbox'} checked={pain.has(Pain.Sharp)} onClick={()=>{togglePain(Pain.Sharp)}} />
+          Sharp
         </label>
       </form>
       <div className="card">
